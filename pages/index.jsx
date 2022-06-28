054cd50d0564f7d243d940ea6e63739a793a805d
Abdullah Ansari <aa9272137@gmail.com>
2022-06-28 13:56:54 +0500

    added meta viewport

--- pages/index.jsx
+++ pages/index.jsx
@@ -56,6 +56,7 @@ const Home = () => {
         <Head>
           <title>Twin Brain</title>
           <meta name="description" content="Twin Brain" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
