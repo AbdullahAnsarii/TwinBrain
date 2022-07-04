632293f44f441354aedf7e5ba48ba3ddcb88cff1
Abdullah Ansari <aa9272137@gmail.com>
2022-07-04 17:41:26 +0500

    Update how-to-pay.jsx

--- pages/how-to-pay.jsx
+++ pages/how-to-pay.jsx
@@ -1,7 +1,8 @@
 import { Col, Container, Row } from "react-bootstrap";
 import Head from "next/head";
 import styles from '../styles/HowToPay.module.scss';
-import { Breadcrumb, Card, Image, Spin } from "antd";
+import { Breadcrumb, Descriptions } from "antd";
+import Link from "next/link";
 import router from "next/router";
 import { Slide } from "react-awesome-reveal";
 const HowToPay = () => {
@@ -21,29 +22,28 @@ const HowToPay = () => {
                 </Breadcrumb>
             </div>
             <Container className="my-5">
-                <h2 className="text-center mb-6">Paying Fees and Terms
-                </h2>
 
-                <p><strong>Payments: </strong>We offer a number of ways to help you pay for your services with TwinBrain:</p>
-                <p>(a) Bank transfer is our preferred payment method:</p>
-                <p><strong>Bank Name: </strong>CashPlus,<strong> Sort Code: </strong>087199, Account<strong> number:</strong> 02926739.<strong> Account name: </strong>Twin Brain Ltd.</p>
-                <p><strong>Payment reference:</strong> Your initials, surname, and date you received the service e.g.,J. Kane23/06/16.</p>
-                <p><strong>Payment via Insurance Firms:</strong></p>
+                <Descriptions title="Paying Fees and Terms" size="small" bordered column={1}>
+                    <Descriptions.Item label="Payments">We offer a number of ways to help you pay for your services with TwinBrain</Descriptions.Item>
+                    <Descriptions.Item><b>Bank transfer is our preferred payment method</b></Descriptions.Item>
+                    <Descriptions.Item label="Bank Name">CashPlus</Descriptions.Item>
+                    <Descriptions.Item label="Sort Code">087199</Descriptions.Item>
+                    <Descriptions.Item label="Account Number">02926739</Descriptions.Item>
+                    <Descriptions.Item label="Account Name">Twin Brain Ltd.</Descriptions.Item>
+                    <Descriptions.Item label="Payment Reference">Your initials, surname, and date you received the service e.g. J. Kane, 23/06/16</Descriptions.Item>
 
-                <p>Therapy, Counselling, Psychology, and some other services can be paid via all health insurance firms including but not limited to:- Standard Life, Cigna, AVIVA, AXA PPP, BUPA International, Vitality (PruHealth). We require your full names, your address with full postcode (if applicable), a name of your Insurer, your Insurance Policy Number, your Authorisation Number from your Insurer, and the number of sessions author authorized Insurer.</p>
-                <p><strong>Note:</strong> BUPA UK is only accepted by some of our therapists so contact us.</p>
-                <p><strong>Paying by Direct Debit:</strong> Clients can set up a direct debit payment using their bank.</p>
-                <p><strong>Paying with debit or credit card:</strong> Clients can pay via <a href="#">Paypal </a> or <a href="#">‘Stripe’</a>. They will charge you a small fee.</p>
-
-                <p><strong>Terms and conditions (inclusive of the above)</strong></p>
-                <p><strong>Payments for Counselling, therapy and treatment services: </strong> All face to face, telephone, email and video-based services are paid in advance. This can be via bank transfer or  PayPal. A receipt will be emailed to you.
-                </p>
-                <p><strong>Travel Fees: </strong>Please note that home and school visits requiring our therapist to use a public or private transport will incur an additional travel fee to reflect the therapist’s expenses and time away from the clinic.</p>
-
-                <p><strong>Cancellation policy:</strong> Services once booked are to be paid in full and no refund for canceled or missed sessions with or without notice.</p>
-
-                <p><strong>Waiting rooms:</strong> Please note that the waiting rooms in some of our offices or clinics are closed evenings and weekends, so clients will be asked to meet their Counsellors or therapists service deliverer (e.g. therapist) at the exact appointment time.</p>
-
+                    <Descriptions.Item><b>Payment via Insurance Firms</b></Descriptions.Item>
+                    <Descriptions.Item>Therapy, Counselling, Psychology, and some other services can be paid via all health insurance firms including but not limited to:- Standard Life, Cigna, AVIVA, AXA PPP, BUPA International, Vitality (PruHealth). We require your full names, your address with full postcode (if applicable), a name of your Insurer, your Insurance Policy Number, your Authorisation Number from your Insurer, and the number of sessions author authorized Insurer.</Descriptions.Item>
+                    <Descriptions.Item label="Note">BUPA UK is only accepted by some of our therapists so contact us.</Descriptions.Item>
+                    <Descriptions.Item label="Paying by Direct Debit">Clients can set up a direct debit payment using their bank.</Descriptions.Item>
+                    <Descriptions.Item label="Paying with debit or credit card">Clients can pay via <b>Paypal </b> or <b>Stripe</b>. They will charge you a small fee.</Descriptions.Item>
+                </Descriptions>
+                <Descriptions title="Terms and conditions (inclusive of the above)" size="small" bordered column={1}>
+                    <Descriptions.Item label="Payments for Counselling, therapy and treatment services">All face to face, telephone, email and video-based services are paid in advance. This can be via bank transfer or  PayPal. A receipt will be emailed to you.</Descriptions.Item>
+                    <Descriptions.Item label="Travel Fees">Please note that home and school visits requiring our therapist to use a public or private transport will incur an additional travel fee to reflect the therapist’s expenses and time away from the clinic.</Descriptions.Item>
+                    <Descriptions.Item label="Cancellation Policy">Services once booked are to be paid in full and no refund for canceled or missed sessions with or without notice.</Descriptions.Item>
+                    <Descriptions.Item label="Waiting Rooms"> Please note that the waiting rooms in some of our offices or clinics are closed evenings and weekends, so clients will be asked to meet their Counsellors or therapists service deliverer (e.g. therapist) at the exact appointment time.</Descriptions.Item>
+                </Descriptions>
             </Container>
         </>
     )
